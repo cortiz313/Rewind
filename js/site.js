4907aaaa21acae177a8a24773b76895e7ff8d887
Christian Ortiz <c.ortiz313@gmail.com>
2022-07-20 22:06:44 -0400

    added the first functions to js

---
+++ js/site.js
@@ -0,0 +1,21 @@
+// Get the string from the page
+// Controller function
+function getValues(){
+
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+
+
+}
+
+// Reverse the string
+// Logic Function
+function reverseString(userString){
+
+}
+
+// Display the reversed string to the user
+// View Function
+function displayString(){
+
+}
