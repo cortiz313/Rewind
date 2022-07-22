bd02a5d43fb04016b3d4622cf6b5260b754516e6
Christian Ortiz <c.ortiz313@gmail.com>
2022-07-21 20:47:16 -0400

    Almost completed
    all functionality is done. js, css complete, just needs function breakdown.

--- js/site.js
+++ js/site.js
@@ -2,20 +2,32 @@
 // Controller function
 function getValues(){
 
+    document.getElementById("alert").classList.add("invisible");
     let userString = document.getElementById("userString").value;
     let revString = reverseString(userString);
-
-
+    displayString(revString);
 }
 
 // Reverse the string
 // Logic Function
 function reverseString(userString){
 
+    let revString = "";
+
+    // reverse a string using a for loop
+    for(let i = userString.length -1; i >= 0; i--)
+        revString += userString[i];
+    return revString;
+
 }
 
 // Display the reversed string to the user
 // View Function
-function displayString(){
+function displayString(revString){
 
+    // Write to the page
+    document.getElementById("msg").innerHTML = `Your reversed string is: ${revString} `;
+
+    // Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
